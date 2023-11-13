ba864281a40b41451a038a5f9e788badf1ab6dfa
NikitaVarsan <NekitVars@icloud.com>
2023-11-13 18:38:47 +0100

    Homework number 8

--- hm1.py
+++ hm1.py
@@ -1,26 +1,41 @@
-lst = [12, 3, 4, 10]
-if len(lst) > 1:
-    last_element = lst.pop()
-    lst.insert(0, last_element)
-print(lst)
+List = [0, 1, 7, 2, 4, 8]
+if not List:
+    result = 0
+else:
+    even_sum = 0
+    for i in range(0, len(List), 2):
+        even_sum += List[i]
+    result = even_sum * List[-1]
+print(result)
 
 
-lst = [1]
-if len(lst) > 1:
-    last_element = lst.pop()
-    lst.insert(0, last_element)
-print(lst)
+List = [1, 3, 5]
+if not List:
+    result = 0
+else:
+    even_sum = 0
+    for i in range(0, len(List), 2):
+        even_sum += List[i]
+    result = even_sum * List[-1]
+print(result)
 
 
-lst = []
-if len(lst) > 1:
-    last_element = lst.pop()
-    lst.insert(0, last_element)
-print(lst)
+List = [6]
+if not List:
+    result = 0
+else:
+    even_sum = 0
+    for i in range(0, len(List), 2):
+        even_sum += List[i]
+    result = even_sum * List[-1]
+print(result)
 
-lst = [12, 3, 4, 10, 8]
-if len(lst) > 1:
-    last_element = lst.pop()
-    lst.insert(0, last_element)
-
-print(lst)
+List = []
+if not List:
+    result = 0
+else:
+    even_sum = 0
+    for i in range(0, len(List), 2):
+        even_sum += List[i]
+    result = even_sum * List[-1]
+print(result)
