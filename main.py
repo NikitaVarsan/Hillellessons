23870257c63221fc0edc00ba3008789b63e18727
NikitaVarsan <NekitVars@icloud.com>
2023-11-13 18:56:26 +0100

    Homework number 9

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-x = list(range(1 , 111))
-print(x)
+import random
+my_list = [random.randint(0,9 )
+for i in range(random.randint(3, 10))]
+
+
+first_number = my_list[0]
+third_number = my_list[2]
+second_from_end_number = my_list[-2]
+
+print(my_list)
+print(first_number, third_number, second_from_end_number)
